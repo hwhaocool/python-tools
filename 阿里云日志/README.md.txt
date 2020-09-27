296b7af38e9e825f36109a51c9ecdd326cbfbb0f
huangwei <huangwei@fanggeek.com>
2020-09-27 18:10:14 +0800

    提交 阿里云日志 小工具

---
+++ 阿里云日志/README.md.txt
@@ -0,0 +1,8 @@
+
+
+## aliyun-log-get-content.py
+作用就是：
+先从阿里云日志系统下载日志文件 `xxx.log`  
+然后使用这个工具，提取其中的 `content` 字段，保存为新的文件
+
+
---
+++ 阿里云日志/aliyun-log-get-content.py
@@ -0,0 +1,45 @@
+#coding: utf-8
+
+import os
+import json
+import time
+
+import sys  
+
+reload(sys)  
+sys.setdefaultencoding('utf8')   
+
+
+ali_log_file = raw_input("input full file path:")
+
+ali_log_file_path, ali_log_file_name =  os.path.split(os.path.realpath(ali_log_file))
+
+print("分割之后是： ")
+print(ali_log_file_path)
+print(ali_log_file_name)
+
+
+f = open(ali_log_file, "r")
+
+file_name = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) 
+
+# 新的文件完整路径
+new_file_name = os.path.join(ali_log_file_path, file_name)
+print(new_file_name)
+
+gc_log = open(new_file_name + ".log", "w")
+
+for data in f.readlines():
+    log = json.loads(data)
+
+    content = log["content"]
+
+    # print(content)
+
+    gc_log.write(content + "\n")
+    # break
+
+f.close()
+
+
+gc_log.close()
